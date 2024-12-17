f8915d4ca25b2a6044c22c7f4a10157ce1836f13
coldousedbird <coldousedbird>
2024-12-17 22:41:26 +0300

    eza aliases; telescope-fb config; nvim file struct

--- nvim/.config/nvim/lua/plugins/telescope-file-browser.lua
+++ nvim/.config/nvim/lua/plugins/telescope-file-browser.lua
@@ -2,7 +2,19 @@ return {
   "nvim-telescope/telescope-file-browser.nvim",
   dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
   config = function()
-    hijack_netrw = true
+    require("telescope").setup {
+        extensions = {
+        file_browser = {
+          depth = 1,
+          grouped = true,
+          git_status = true,
+          collapse_dirs = true,
+          follow_symlinks = true,
+          hidden = { file_browser = true, folder_browser = true },
+          hijack_netrw = true,
+        },
+      },
+    }
     vim.keymap.set("n", "<C-b>", ":Telescope file_browser<CR>")
   end
 }
