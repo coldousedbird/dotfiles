120f8ae7e16211595a4b2c9fe50d54d796092ab5
coldousedbird <coldousedbird>
2024-12-18 07:06:21 +0300

    edited mappings

--- nvim/.config/nvim/lua/configs/mappings.lua
+++ nvim/.config/nvim/lua/configs/mappings.lua
@@ -1,29 +1,39 @@
 local wk = require('which-key')
 
 wk.add({
+  -- Editing
   { "<C-,>", "$a,<Esc>", desc = "Comma at end" },
   { "<C-Return>", "$a<CR><Esc>", desc = "New line at end" },
-  { "<C-S-h>", "<cmd>vertical resize +6<CR>", desc = "Resize window left" },
-  { "<C-S-j>", "<cmd>resize +3<CR>", desc = "Resize window down" },
-  { "<C-S-k>", "<cmd>resize -3<CR>", desc = "Resize window up" },
-  { "<C-S-l>", "<cmd>vertical resize -6<CR>", desc = "Resize window right" },
+  { "==", "gg=G", desc = "Reindent file" },
+  { "J", "mzJ`z", desc = "Join line with next" },
+
+  -- Menu
+  { "<leader>l", "<cmd>Lazy home<CR>", desc = "Open Lazy main menu" },
+  -- ??? { "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", desc = "Substitute word under cursor" },
+
+  -- Navigation
   { "<C-d>", "<C-d>zz", desc = "Scroll down and center" },
+  { "<C-u>", "<C-u>zz", desc = "Scroll up and center" },
+
+  -- Search
+  { "N", "Nzzzv", desc = "Previous search result and center" },
+  { "n", "nzzzv", desc = "Next search result and center" },
+
+  -- Tabs
+  { "<leader>t", ":tabe<CR>", desc = "Spawn new empty tab"},
+  { "<t>", ":tabn<CR>", desc = "Switch to next tab"},
+  { "<S-t>", ":tabp<CR>", desc = "Switch to previous tab"},
+
+  -- Windows
+  { "<leader>w", ":vsplit<CR>", desc = "Spawn new window (inside tab)"},
   { "<C-h>", "<C-w><C-h>", desc = "Move to left window" },
   { "<C-j>", "<C-w><C-j>", desc = "Move to below window" },
   { "<C-k>", "<C-w><C-k>", desc = "Move to above window" },
   { "<C-l>", "<C-w><C-l>", desc = "Move to right window" },
-  { "<C-u>", "<C-u>zz", desc = "Scroll up and center" },
-  -- { "<leader>Y", '"*Y', desc = "Yank line to system clipboard" },
-  { "<leader>l", "<cmd>Lazy home<CR>", desc = "Open Lazy main menu" },
-  { "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>", desc = "Substitute word under cursor" },
-  { "<leader>y", '"*y', desc = "Yank to system clipboard" },
-  { "==", "gg=G", desc = "Reindent file" },
-  { "J", "mzJ`z", desc = "Join line with next" },
-  { "N", "Nzzzv", desc = "Previous search result and center" },
-  { "n", "nzzzv", desc = "Next search result and center" },
-  { "<C-s>", ":tabe<CR>", desc = "Spawn new empty tab"},
-  { "<C-n>", ":tabn<CR>", desc = "Switch to next tab"},
-  { "<C-p>", ":tabp<CR>", desc = "Switch to previous tab"},
+  { "<C-S-h>", "<cmd>vertical resize +6<CR>", desc = "Resize window left" },
+  { "<C-S-j>", "<cmd>resize +3<CR>", desc = "Resize window down" },
+  { "<C-S-k>", "<cmd>resize -3<CR>", desc = "Resize window up" },
+  { "<C-S-l>", "<cmd>vertical resize -6<CR>", desc = "Resize window right" },
 })
 
 wk.add({
