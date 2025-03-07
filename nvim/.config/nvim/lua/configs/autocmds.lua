6d96758c475dbd1eacce16b80c96b040d1af9b3b
coldousedbird <coldousedbird@gmail.com>
2025-03-07 16:39:44 +0300

    nvim autocmd: transparent background

--- nvim/.config/nvim/lua/configs/autocmds.lua
+++ nvim/.config/nvim/lua/configs/autocmds.lua
@@ -1,3 +1,11 @@
+-- make windows transparent
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
+
 -- Help window is vertical
 vim.api.nvim_create_autocmd('BufWinEnter', {
   pattern = { '*.txt' },
--- nvim/.config/nvim/lua/plugins/everforest.lua
+++ nvim/.config/nvim/lua/plugins/everforest.lua
@@ -1,8 +1,10 @@
 return {
     "neanias/everforest-nvim",
-    version = false, lazy = false, priority = 1000, 
-    config = function() 
-      require("everforest").setup({}) 
+    version = false, lazy = false, priority = 1000,
+    config = function()
+      require("everforest").setup({
+        transparent_background_level = 0,
+      })
       vim.cmd.colorscheme "everforest"
-    end 
+    end
 }
