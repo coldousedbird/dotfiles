9fb18e875d0093b09d344dd52620d6c6f443a284
Dmitry Belousov <belousov@axiomatika.ru>
2025-04-04 17:31:47 +0300

    nvim: added jinja to treesitter ensure

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"sway", "lua", "asm", "bash", "vim", "regex", "dockerfile", "yaml", "c", "cpp", "rust", "html", "css", "csv", "sql", "markdown", "markdown_inline", "comment", "jsonc" },
+        ensure_installed = {"sway", "lua", "asm", "bash", "vim", "regex", "dockerfile", "yaml", "c", "cpp", "rust", "html", "css", "csv", "sql", "markdown", "markdown_inline", "jinja", "jinja_inline", "comment", "jsonc" },
         sync_install = false,
         higlight = { enable = true },
         indent = { enable = true },
