439950354eb65b725d3c5c3b798bf10fc11742fb
coldousedbird <coldousedbird@gmail.com>
2025-09-05 18:00:51 +0300

    yazi stuff

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,3 +1,12 @@
 function Linemode:file_info()
   return string.format("%s %7s %14s", self:permissions(), self:size(), self:mtime())
 end
+
+-- require("current-size"):setup({
+--     equal_ignore = {"~","/","/home"}, -- full path match
+-- 	-- sub_ignore = {"~/deskenv/master","~/deskenv/dev"} -- sub path match
+-- })
+-- 
+require("fg"):setup({
+    default_action = "menu", -- nvim, jump
+})
